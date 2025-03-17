843c9ac267ea9916efcce0151137b1c0a16ac1b0
Raamprathap <raamprathap17242@gmail.com>
2025-03-17 12:39:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import { getFirestore, collection, getDocs, query, orderBy, limit, onSnapshot, a
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
